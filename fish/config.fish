7fcf3b617f781951c5a3179a9efafcaf2482b23f
KauaRios <kauarios49@gmail.com>
2026-05-03 15:07:58 -0300

    correçao de diversos bugs

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,12 @@
 # ~/.config/fish/config.fish
 
 # 1. Remove a mensagem padrão chata do Fish
+
+
+
 set -g fish_greeting
+
+
 
 # 2. Tenta carregar as otimizações do CachyOS (se o usuário estiver no CachyOS)
 if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
@@ -9,7 +14,7 @@ if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
 end
 
 # 3. GARANTE o Fastfetch em qualquer distro (Arch, Fedora, VM, etc)
-# Só roda se o fastfetch estiver instalado e se NÃO estiver no CachyOS 
+# Só roda se o fastfetch estiver instalado e se NÃO estiver no CachyOS
 # (para evitar que apareça duas vezes no CachyOS)
 if command -q fastfetch
     if not test -f /usr/share/cachyos-fish-config/cachyos-config.fish
